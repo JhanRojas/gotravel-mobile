818cce496d5d8cfc4ad5277214a9675e4c857015
RaulGodoy82 <raul_godoy82@hotmail.com>
2016-10-08 17:27:01 -0500

    Airline and Flight created

---
+++ app/src/main/java/model/Airline.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by RGodoy on 08/10/2016.
+ */
+
+public class Airline {
+}
---
+++ app/src/main/java/model/Flight.java
@@ -0,0 +1,9 @@
+package model;
+
+/**
+ * Created by RGodoy on 08/10/2016.
+ */
+
+public class Flight {
+
+}
