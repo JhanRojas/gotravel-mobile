7c96aef2665d58b4254be42e113c4e2ea866f5c0
CarlosMakito <carlos.miyashiro.tsukazan@gmail.com>
2016-10-08 16:41:07 -0500

    Creacion de clase restaurant v2

--- app/src/main/java/model/restaurant.java
+++ app/src/main/java/model/restaurant.java
@@ -1,5 +1,7 @@
 package model;
 
+import org.w3c.dom.Text;
+
 /**
  * Created by u201400194 on 8/10/2016.
  */
@@ -7,13 +9,51 @@ package model;
 public class restaurant {
 
     private String name;
+    private String description;
+    private Integer level;
+    private Text websiteurl;
+    private Text logourl;
+    private int status;
+    private int service;
 
-    public restaurant(String name){
+    public restaurant() {
+    }
 
+    public restaurant(int service, int status, Text logourl, Text websiteurl, Integer level, String description, String name) {
+        this.service = service;
+        this.status = status;
+        this.logourl = logourl;
+        this.websiteurl = websiteurl;
+        this.level = level;
+        this.description = description;
         this.name = name;
     }
 
+    public String getDescription() {
+        return description;
+    }
 
+    public int getService() {
+        return service;
+    }
 
+    public Text getLogourl() {
+        return logourl;
+    }
 
+    public int getStatus() {
+        return status;
+    }
+
+    public Text getWebsiteurl() {
+        return websiteurl;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
