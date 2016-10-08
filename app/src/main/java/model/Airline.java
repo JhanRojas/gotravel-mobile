5e1ecb69c7bff53e333d6b4acfac51d2d1a04167
RaulGodoy82 <raul_godoy82@hotmail.com>
2016-10-08 18:01:40 -0500

    Get / Set created for Airline and Flight

--- app/src/main/java/model/Airline.java
+++ app/src/main/java/model/Airline.java
@@ -5,4 +5,41 @@ package model;
  */
 
 public class Airline {
+
+    private int airlineId;
+    private String name;
+    private String description;
+
+    public Airline(int airlineId, String name, String description) {
+        this.airlineId=airlineId;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getAirlineId() {
+        return airlineId;
+    }
+
+    public void setAirlineId(int airlineId) {
+        this.airlineId = airlineId;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
+
